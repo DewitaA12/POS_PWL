ebdc53c9a624f9ab4fe81d615d1bf38ea19c3f2a
DewitaA12 <xsclthzn@gmail.com>
2025-03-12 01:31:47 +0700

    Mengupdate 1 data menggunakan Query Builder

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -9,12 +9,15 @@ class KategoriController extends Controller
 {
     public function index()
 {
-    $data = [
-        'kategori_kode' => 'KCT',
-        'kategori_nama' => 'Produk Kecantikan',
-        'created_at' => now()
-    ];
-    DB::table('m_kategori')->insert($data);
-    return 'Insert data baru berhasil';
+    // $data = [
+    //     'kategori_kode' => 'KCT',
+    //     'kategori_nama' => 'Produk Kecantikan',
+    //     'created_at' => now()
+    // ];
+    // DB::table('m_kategori')->insert($data);
+    // return 'Insert data baru berhasil';
+
+    $row = DB::table('m_kategori')->where('kategori_kode', 'KCT')->update(['kategori_nama' => 'Skincare']);
+    return 'Update data berhasil.jumlah data yang diupdate: ' .$row. ' baris';
 }
 }
