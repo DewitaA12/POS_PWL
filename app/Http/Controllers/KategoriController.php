454df330f6177567d6c3322c902abb60614911d3
DewitaA12 <xsclthzn@gmail.com>
2025-03-12 03:59:42 +0700

    Menghapus data menggunakan Query Builder

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -17,7 +17,11 @@ class KategoriController extends Controller
     // DB::table('m_kategori')->insert($data);
     // return 'Insert data baru berhasil';
 
-    $row = DB::table('m_kategori')->where('kategori_kode', 'KCT')->update(['kategori_nama' => 'Skincare']);
-    return 'Update data berhasil.jumlah data yang diupdate: ' .$row. ' baris';
+    // $row = DB::table('m_kategori')->where('kategori_kode', 'KCT')->update(['kategori_nama' => 'Skincare']);
+    // return 'Update data berhasil.jumlah data yang diupdate: ' .$row. ' baris';
+
+    $row = DB::table('m_kategori')->where('kategori_kode', 'KCT')->delete();
+    return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row . ' baris';
+
 }
 }
