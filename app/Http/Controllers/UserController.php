e5dabe697548cd7ce62b98f77d873b5bddd6bbcd
DewitaA12 <xsclthzn@gmail.com>
2025-03-19 06:28:03 +0700

    Attributes Changes : isDirty() isClean() implement function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,57 +10,31 @@ class UserController extends Controller
 {
     public function index()
     {
-        //Retrieving Single Models
-        //$user = UserModel::find(1); 
-        // //$user = UserModel::where('level_id', 1)->first();
-        // // $user = UserModel::firstWhere('level_id', 1);
-        // $user = UserModel::findOr(20,['username', 'nama'], function(){
-        //     abort(404);
-        // });
-
-        //Not Found Exceptions
-        //$user = UserModel::findOrFail(1);
-        //$user = UserModel::where('username', 'manager9')->firstorFail();
-
-        //Retrieving Agregrates
-        // $user = UserModel::where('level_id',2)->count();
-        // dd($user);
-
-        //Retrieving or Creating Model
-        // $user = UserModel :: firstOrCreate(
-        //     [
-        //     'username' => 'manager',
-        //     'nama' => 'Manager',
-        //     ],
-        // );
-
-        // $user = UserModel :: firstOrCreate(
-        //     [
-        //         'username' => 'manager22',
-        //         'nama' => 'Manager Dua Dua',
-        //         'password' => Hash::make('12345'),
-        //         'level_id' => 2
-        //      ],
-        // );
-
-        // $user = UserModel :: firstOrNew(
-        //     [
-        //     'username' => 'manager',
-        //     'nama' => 'Manager',
-        //     ],
-        // );
-
-        $user = UserModel :: firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+        $user = UserModel::create([
+            'username' => 'manager55',
+            'nama' => 'Manager55',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+        
+        $user->username = 'manager56';
+        
+        $user->isDirty(); // true
+        $user->isDirty('username'); // true
+        $user->isDirty('nama'); // false
+        $user->isDirty(['nama', 'username']); // true
+        
+        $user->isClean(); // false
+        $user->isClean('username'); // false
+        $user->isClean('nama'); // true
+        $user->isClean(['nama', 'username']); // false
+        
         $user->save();
-
-        return view('user', ['data' => $user]);
+        
+        $user->isDirty(); // false
+        $user->isClean(); // true
+        
+        dd($user->isDirty());
     }
 
     public function jumlahPengguna()
