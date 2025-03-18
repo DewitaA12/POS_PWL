dc1f1d1e36abb9a00edc9b14d66385b0d97b65af
DewitaA12 <xsclthzn@gmail.com>
2025-03-19 04:15:08 +0700

    Menggunakan findOr untuk menangani user yang tidak ditemukan dan memunculkan 404

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,8 +11,11 @@ class UserController extends Controller
     public function index()
     {
         //$user = UserModel::find(1); 
-        //$user = UserModel::where('level_id', 1)->first();
-        $user = UserModel::firstWhere('level_id', 1);
+        // //$user = UserModel::where('level_id', 1)->first();
+        // // $user = UserModel::firstWhere('level_id', 1);
+        $user = UserModel::findOr(20,['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
