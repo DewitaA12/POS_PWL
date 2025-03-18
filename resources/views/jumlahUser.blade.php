6766fa58350375ba44e109cea679469482dd4e0e
DewitaA12 <xsclthzn@gmail.com>
2025-03-19 05:04:20 +0700

    Membuat view untuk menampilkan tabel jumlah pengguna

---
+++ resources/views/jumlahUser.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data User</title>
+</head>
+<body>
+    <h1>Data User</h1>
+
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <td>Jumlah Pengguna</td>
+        </tr>
+        <tr>
+            <td>{{ $jumlahPengguna }}</td>
+        </tr>
+    </table>
+</body>
+</html>
