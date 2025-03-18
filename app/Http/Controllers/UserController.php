c5c61b86892a67b67c0f7a5500d7b98beb77d5ac
DewitaA12 <xsclthzn@gmail.com>
2025-03-19 05:52:27 +0700

    Retrieve or Create Models : firstOrCreate function dengan data yang belum ada sehingga insert data baru

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,12 +27,22 @@ class UserController extends Controller
         // dd($user);
 
         //Retrieving or Creating Model
+        // $user = UserModel :: firstOrCreate(
+        //     [
+        //     'username' => 'manager',
+        //     'nama' => 'Manager',
+        //     ],
+        // );
+
         $user = UserModel :: firstOrCreate(
             [
-            'username' => 'manager',
-            'nama' => 'Manager',
-            ],
+                'username' => 'manager22',
+                'nama' => 'Manager Dua Dua',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+             ],
         );
+
         return view('user', ['data' => $user]);
     }
 
