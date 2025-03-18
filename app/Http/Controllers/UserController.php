3235e07114c4905b21993578bcf4e81ea61ed186
DewitaA12 <xsclthzn@gmail.com>
2025-03-19 06:05:57 +0700

    Menambahkan fungsi save, untuk menyimpan perubahan ke database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -58,6 +58,7 @@ class UserController extends Controller
                 'level_id' => 2
             ],
         );
+        $user->save();
 
         return view('user', ['data' => $user]);
     }
