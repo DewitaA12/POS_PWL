728418a956f584e6801013362fc562ad0b6a3f07
DewitaA12 <xsclthzn@gmail.com>
2025-03-19 05:05:38 +0700

    Membuat route controller-view untuk menampilkan tabel jumlah pengguna

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ Route::get('/', function () {
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/jumlah-pengguna', [UserController::class, 'jumlahPengguna']);
