b12610eb2420dcc3888835f93f64d12124b2a9a6
DewitaA12 <xsclthzn@gmail.com>
2025-03-19 06:03:38 +0700

    Retrieve or Create Models : firstOrNew function dengan data yang baru

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,10 +43,19 @@ class UserController extends Controller
         //      ],
         // );
 
+        // $user = UserModel :: firstOrNew(
+        //     [
+        //     'username' => 'manager',
+        //     'nama' => 'Manager',
+        //     ],
+        // );
+
         $user = UserModel :: firstOrNew(
             [
-            'username' => 'manager',
-            'nama' => 'Manager',
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
             ],
         );
 
