1ca7b1d01c4143bb02e13c06537f2deb9923be0f
DewitaA12 <xsclthzn@gmail.com>
2025-03-19 04:39:09 +0700

    Retrieving Agregrates : count function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,14 +10,21 @@ class UserController extends Controller
 {
     public function index()
     {
+        //Retrieving Single Models
         //$user = UserModel::find(1); 
         // //$user = UserModel::where('level_id', 1)->first();
         // // $user = UserModel::firstWhere('level_id', 1);
         // $user = UserModel::findOr(20,['username', 'nama'], function(){
         //     abort(404);
         // });
+
+        //Not Found Exceptions
         //$user = UserModel::findOrFail(1);
-        $user = UserModel::where('username', 'manager9')->firstorFail();
+        //$user = UserModel::where('username', 'manager9')->firstorFail();
+
+        //Retrieving Agregrates
+        $user = UserModel::where('level_id',2)->count();
+        dd($user);
         return view('user', ['data' => $user]);
     }
 }
