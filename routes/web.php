f003f65a1590738bcf93912ab4b496d92b3aed39
DewitaA12 <xsclthzn@gmail.com>
2025-03-19 09:01:03 +0700

    Menambahkan route untuk welcome

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\WelcomeController;
 use Database\Seeders\KategoriSeeder;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/jumlah-pengguna', [UserController::class, 'jumlahPengguna']);
+Route::get('/', [WelcomeController::class, 'index']);
