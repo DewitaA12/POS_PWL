9020d37a1b7875bf9b9f74a4a77277a6029ebecd
DewitaA12 <xsclthzn@gmail.com>
2025-03-19 10:40:20 +0700

    Menambahkanfungsi tambah untuk data user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
@@ -10,28 +11,18 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
-        ]);
-        
-        $user->username = 'manager12';
-        
-        $user->save();
-        
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged(['username', 'level_id']); // true
-        $user->wasChanged('nama'); // false
-        
-        dd($user->wasChanged(['nama', 'username'])); // true
+       $user = UserModel::all();
+       return view('user', ['data' => $user]);
     }
 
     public function jumlahPengguna()
     {
         $jumlahPengguna = UserModel::where('level_id', 2)->count();
         return view('jumlahUser', ['jumlahPengguna' => $jumlahPengguna]);
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
     }
 }
