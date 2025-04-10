9ab348362c9a77af9c0f405c74522a61036a8b05
DewitaA12 <xsclthzn@gmail.com>
2025-04-10 17:01:57 +0700

    Menambahkan fungsi hapus untuk data user berdasarkan id

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -60,4 +60,11 @@ class UserController extends Controller
     return redirect('/user');
 }
 
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
+    }
 }
