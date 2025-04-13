b00ca51e67c87c4d3b732c705d8e4c45b81e3676
DewitaA12 <xsclthzn@gmail.com>
2025-04-13 17:35:51 +0700

    Membuat model level dengan konfigurasi nama tabel dan primary key

---
+++ app/Models/LevelModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LevelModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'levels'; // Match the new table name
+    protected $primaryKey = 'level_id'; // Match the PK name
+    protected $fillable = ['level_kode', 'level_nama'];
+}
