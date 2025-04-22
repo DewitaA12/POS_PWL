264dd2302af1a9e837239734cfb620b2fa8724f3
DewitaA12 <xsclthzn@gmail.com>
2025-04-22 20:19:24 +0700

    Grouping routes dengan prefix user

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,19 @@ use Database\Seeders\KategoriSeeder;
 use Illuminate\Support\Facades\Route;
 
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']); // /user
+    Route::post('/list', [UserController::class, 'list']); // /user/list
+    Route::get('/tambah', [UserController::class, 'tambah']); // /user/tambah
+    Route::get('/create', [UserController::class, 'create']); // /user/create
+    Route::get('/{id}/edit', [UserController::class, 'edit']); // /user/create
+    Route::post('/', [UserController::class, 'tambah_simpan']); // /user (POST)
+    Route::get('/ubah/{id}', [UserController::class, 'ubah']); // /user/ubah/1
+    Route::put('/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']); // /user/ubah_simpan/1
+    Route::get('/hapus/{id}', [UserController::class, 'hapus']); // /user/hapus/1
+    Route::get('/test-relasi', [UserController::class, 'testRelasi']);
+    Route::get('/jumlah-pengguna', [UserController::class, 'jumlahPengguna']);
+});
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/jumlah-pengguna', [UserController::class, 'jumlahPengguna']);
-Route::get('/', [WelcomeController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
