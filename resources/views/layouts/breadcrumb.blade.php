afa42f6a6c148cdb507bd6a49799099bc14156f1
DewitaA12 <xsclthzn@gmail.com>
2025-04-23 00:00:19 +0700

    Utilize escaping blade for security

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -4,13 +4,17 @@
       <div class="col-sm-6"><h1>{{ $breadcrumb->title }}</h1></div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-          @foreach ($breadcrumb->list as $key => $value)
-            @if ($key == count($breadcrumb->list) - 1)
-              <li class="breadcrumb-item active">{{ $value }}</li>
-            @else
-              <li class="breadcrumb-item">{{ $value }}</li>
-            @endif
-          @endforeach
+          @if (isset($breadcrumb->list) && is_array($breadcrumb->list))
+            @foreach ($breadcrumb->list as $key => $value)
+              @if (isset($value['title']) && is_string($value['title'])) 
+                <li class="breadcrumb-item {{ $key == count($breadcrumb->list) - 1 ? 'active' : '' }}">
+                  {{ htmlspecialchars($value['title'], ENT_QUOTES, 'UTF-8') }}
+                </li>
+              @endif
+            @endforeach
+          @else
+            <li class="breadcrumb-item">No breadcrumbs available</li>
+          @endif
         </ol>
       </div>
     </div>
