a6f1772a59544d5676334f9485fb397f4f290a2c
DewitaA12 <xsclthzn@gmail.com>
2025-05-31 17:31:41 +0700

    Update sow ajax with pop up card

--- resources/views/user/show_ajax.blade.php
+++ resources/views/user/show_ajax.blade.php
@@ -1,37 +1,99 @@
-<div class="modal-dialog">
+@empty($user)
+<div id="modal-master" class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title">Detail User</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Kesalahan</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
         <div class="modal-body">
-            <table class="table table-bordered table-striped table-hover table-sm">
-                <tr>
-                    <th>ID</th>
-                    <td>{{ $user->user_id }}</td>
-                </tr>
-                <tr>
-                    <th>Level</th>
-                    <td>{{ $user->level->level_nama }}</td>
-                </tr>
-                <tr>
-                    <th>Username</th>
-                    <td>{{ $user->username }}</td>
-                </tr>
-                <tr>
-                    <th>Nama</th>
-                    <td>{{ $user->nama }}</td>
-                </tr>
-                <tr>
-                    <th>Password</th>
-                    <td>********</td>
-                </tr>
-            </table>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+            <div class="alert alert-danger">
+                <h5><i class="icon fas fa-ban"></i> Kesalahan!!!</h5>
+                Data yang anda cari tidak ditemukan
+            </div>
+            <a href="{{ url('/user') }}" class="btn btn-warning">Kembali</a>
         </div>
     </div>
-</div>  
+</div>
+@else
+<form id="form-show_ajax">
+    @csrf
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Detail Data User</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <table class="table table-sm table-bordered table-striped">
+                    <tr>
+                        <th class="text-right col-3">Level Pengguna :</th>
+                        <td class="col-9">{{ $user->level->level_nama }}</td>
+                    </tr>
+                    <tr>
+                        <th class="text-right col-3">Username :</th>
+                        <td class="col-9">{{ $user->username }}</td>
+                    </tr>
+                    <tr>
+                        <th class="text-right col-3">Nama :</th>
+                        <td class="col-9">{{ $user->nama }}</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" data-dismiss="modal" class="btn btn-warning">Keluar</button>
+            </div>
+        </div>
+    </div>
+</form>
+<script>
+    $(document).ready(function() {
+        $("#form-show_ajax").validate({
+            rules: {},
+            submitHandler: function(form) {
+                $.ajax({
+                    url: form.action,
+                    type: form.method,
+                    data: $(form).serialize(),
+                    success: function(response) {
+                        if (response.status) {
+                            $('#myModal').modal('hide');
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Berhasil',
+                                text: response.message
+                            });
+                            dataUser.ajax.reload();
+                        } else {
+                            $('.error-text').text('');
+                            $.each(response.msgField, function(prefix, val) {
+                                $('#error-' + prefix).text(val[0]);
+                            });
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Terjadi Kesalahan',
+                                text: response.message
+                            });
+                        }
+                    }
+                });
+                return false;
+            },
+            errorElement: 'span',
+            errorPlacement: function(error, element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight: function(element) {
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function(element) {
+                $(element).removeClass('is-invalid');
+            }
+        });
+    });
+</script>
+@endempty
