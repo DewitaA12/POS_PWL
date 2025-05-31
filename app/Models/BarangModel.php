9d8bb00ea9913150a54c72a476c7f4a9e2d243f4
DewitaA12 <xsclthzn@gmail.com>
2025-05-31 17:36:10 +0700

    Inisialisasi ulang fillabel field

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -12,8 +12,7 @@ class BarangModel extends Model
 
     protected $table = 'm_barang'; // Nama tabel di database
     protected $primaryKey = 'barang_id'; // Primary key dari tabel
-    protected $foreignKey = 'kategori_id';
-    protected $fillable = ['barang_kode', 'barang_nama', 'harga_beli', 'harga_jual']; // Kolom yang boleh diisi secara massal
+    protected $fillable = ['barang_kode', 'barang_nama', 'harga_beli', 'harga_jual', 'kategori_id']; // Kolom yang boleh diisi secara massal
 
     public function kategori(): BelongsTo
     {
